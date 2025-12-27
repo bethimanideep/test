94412cf57a48ba0837b1fe6b53a0ef447cd5ddf9
mbethi <mbethi@revasolutions.com>
2025-12-27 15:53:48 +0530

    o

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors({ origin: true, credentials: true }));
 
 app.get("/set-cookie", (req, res) => {
   res.cookie("username", "manideep", { 
-    httpOnly: false, secure: true, sameSite: "none", partitioned: true
+    httpOnly: false, secure: true, sameSite: "none"
   });
   res.send("Cookie set ✓");
 });
