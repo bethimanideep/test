0f88589ac310887a3bfa3eae7b94c2b32151e352
mbethi <mbethi@revasolutions.com>
2025-12-27 15:30:49 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -6,27 +6,25 @@ const app = express();
 
 app.use(cookieParser());
 
-// allow frontend origin
 app.use(
   cors({
-    origin: true, // <-- change to your site
+    origin: true,
     credentials: true,
   })
 );
 
-// Set cookie
 app.get("/set-cookie", (req, res) => {
   res.cookie("username", "manideep", {
     httpOnly: true,
-    secure: true,          // must be true for cross-site
-    sameSite: "none",      // must be "none" for cross-site
-    maxAge: 1000 * 60 * 5,
+    secure: true,        // must be HTTPS
+    sameSite: "none",    // required for cross-site
+    partitioned: true,   // ⭐ CHIPS magic
+    maxAge: 1000 * 60 * 10
   });
 
-  res.send("Cross-site cookie set!");
+  res.send("Cross-site cookie set with res.cookie + Partitioned");
 });
 
-// Get cookie
 app.get("/get-cookie", (req, res) => {
   const username = req.cookies.username;
   res.send(username ? `Cookie: ${username}` : "No cookie found");
